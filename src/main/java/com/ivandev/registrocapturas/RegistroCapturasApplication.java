1dbb3b159a33c3617b674610a73e96152b03704a
ivangarrido02 <ivangarridodurban02@gmail.com>
2026-03-11 13:27:57 +0100

    Initialize project with Spring Boot and dependencies

---
+++ src/main/java/com/ivandev/registrocapturas/RegistroCapturasApplication.java
@@ -0,0 +1,13 @@
+package com.ivandev.registrocapturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroCapturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroCapturasApplication.class, args);
+	}
+
+}
