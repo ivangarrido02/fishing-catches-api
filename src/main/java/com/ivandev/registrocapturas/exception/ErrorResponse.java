822f9b2438cfee3296d48db1004249353537c1fe
ivangarrido02 <ivangarridodurban02@gmail.com>
2026-03-14 13:34:25 +0100

    Standardize API error response format

--- src/main/java/com/ivandev/registrocapturas/exception/ErrorResponse.java
+++ src/main/java/com/ivandev/registrocapturas/exception/ErrorResponse.java
@@ -1,10 +1,11 @@
 package com.ivandev.registrocapturas.exception;
 
 
-import java.time.LocalDateTime;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Getter;
 
@@ -16,26 +17,25 @@ import lombok.Getter;
  * the specific path where the exception occurred and a list of fields errors. 
  */
 @Getter
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ErrorResponse {
 	private final int status;
 	private final ErrorCode errorCode;
 	private String message;
 	private String path;
 	private List<FieldErrorDetail> errors;
-
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
-	private LocalDateTime timestamp;
+	private Instant timestamp;
 
 	public ErrorResponse(int status, ErrorCode errorCode, String message, String path, List<FieldErrorDetail> errors) {
 		this.status = status;
 		this.errorCode = errorCode;
 		this.message = message;
 		this.path = path;
-		this.timestamp = LocalDateTime.now();
+		this.timestamp = Instant.now().truncatedTo(ChronoUnit.SECONDS);
 		this.errors = errors;
 	}
 	
-	public void setTimestamp(LocalDateTime timestamp) {
+	public void setTimestamp(Instant timestamp) {
 		this.timestamp = timestamp;
 	}
 
